28c25d86d3f832c029c253d657d3ed871765f780
s <96222718+sa6ds@users.noreply.github.com>
2023-04-10 03:11:49 -0400

    Basic Setup

---
+++ src/Components/CardData.js
@@ -0,0 +1,26 @@
+export const CardData = [
+  {
+    title: "codshot",
+    image:
+      "https://cdn0.iconfinder.com/data/icons/sound-ui-collection/512/Sound_Sound_wave_Audio-10-512.png",
+    soundBit: "codshot.mp3",
+    downloadURL:
+      "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg",
+  },
+  {
+    title: "knock",
+    image:
+      "https://cdn0.iconfinder.com/data/icons/sound-ui-collection/512/Sound_Sound_wave_Audio-10-512.png",
+    soundBit: "knock.mp3",
+    downloadURL:
+      "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg",
+  },
+  {
+    title: "ding",
+    image:
+      "https://cdn0.iconfinder.com/data/icons/sound-ui-collection/512/Sound_Sound_wave_Audio-10-512.png",
+    soundBit: "ding.mp3",
+    downloadURL:
+      "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg",
+  },
+];
